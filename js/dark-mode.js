077613ca2adefd1839c7e3139f11ff9088d4db88
Colin Ford <colin.ford@student.csulb.edu>
2022-06-26 02:58:56 -0700

    dark mode uses sessionStorage now

--- js/dark-mode.js
+++ js/dark-mode.js
@@ -1,8 +1,34 @@
 // from css-tricks.com/a-complete-guide-to-dark-mode-on-the-web/
-// and stackoverflow.com/questions/53984830/uncaught-typeerror-cannot-read-property-addeventlistener-of-null-in-javascrip
+// and stackoverflow.com/questions/53984830/uncaught-typeerror-cannot-read-property-addeventlistener-of-null-in-javascript
+// the additional functions were written after reading about sessionStorage, see e.g. MDN
+let light = "light";
+let dark = "dark";
+let mode = "mode";
+
+function setToLight() {	sessionStorage.setItem(mode, light); }
+
+function setToDark() { sessionStorage.setItem(mode, dark); }
+
+function toggleMode() {
+	document.body.classList.toggle("dark-theme");
+}
+
+function doNothing() {}
+
+function checkAndLoad() {
+	if (sessionStorage.getItem(mode)) {
+	  let storedMode = sessionStorage.getItem(mode);
+		let currentModeIsDark = document.body.classList.contains("dark-theme");
+		storedMode == dark && !currentModeIsDark ? toggleMode() : doNothing();
+	} else {
+		setToLight();
+	}
+}
+
 document.addEventListener("DOMContentLoaded", function(event) {
 	const btn = document.querySelector("#dm-toggle");
+	checkAndLoad();
 	btn.addEventListener("click", function() {
-	  document.body.classList.toggle("dark-theme");
+		document.body.classList.toggle("dark-theme") ? setToDark() : setToLight();
 	});
 });
